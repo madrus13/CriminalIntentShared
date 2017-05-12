ade952a2f51d2e9165ee1467e85d604601c20a7d
madrus13 <madrus@bk.ru>
2017-05-13 00:04:56 +0400

    add: Chapter 12 start

---
+++ src/main/java/com/example/sampletest/criminalintent/DataPickerFragment.java
@@ -0,0 +1,22 @@
+package com.example.sampletest.criminalintent;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.DialogFragment;
+
+/**
+ * Created by ms on 12.05.2017.
+ */
+
+public class DataPickerFragment extends DialogFragment {
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        return new AlertDialog.Builder(getActivity())
+                .setTitle(R.string.date_picker_title)
+                .setPositiveButton(android.R.string.ok, null)
+                .create();
+    }
+}
